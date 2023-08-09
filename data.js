40f50a041feb28c967aab739ef5d9361585d9fd7
Ancieta <ancietalea.imp@gmail.com>
2023-08-09 18:18:32 -0300

    PreEntrega3 + Ancieta

---
+++ data.js
@@ -0,0 +1,215 @@
+export const gpu = {
+  rx6600_asus: {
+    nombre: "ASUS Radeon RX 6600",
+    imagen: "../recursos/img/productos/gpu/rx6600/asus-gpu.webp",
+    vram: "8GB",
+    chipset: "Rx 6600",
+    precio: 200,
+    descripcion:
+      "Potente tarjeta gráfica para juegos con 8GB de memoria GDDR6 y soporte para Ray Tracing para una experiencia visual impresionante",
+  },
+  rx6600_gigabyte: {
+    nombre: "GIGABYTE Radeon RX 6600 Gaming OC",
+    imagen: "../recursos/img/productos/gpu/rx6600/gigabyte-gpu.webp",
+    vram: "8GB",
+    chipset: "Rx 6600",
+    precio: 200,
+    descripcion:
+      "Rendimiento excepcional para juegos en resoluciones 1080p, equipada con 8GB de memoria GDDR6 y diseño de triple ventilador",
+  },
+  rx6600_msi: {
+    nombre: "MSI Radeon RX 6600 Gaming X",
+    imagen: "../recursos/img/productos/gpu/rx6600/msi-gpu-transformed.webp",
+    vram: "8GB",
+    chipset: "Rx 6600",
+    precio: 200,
+    descripcion:
+      "Tarjeta gráfica de alta calidad para jugadores exigentes, con sistema de enfriamiento avanzado y 8GB de VRAM",
+  },
+  rx6600_sapphire: {
+    nombre: "Sapphire Pulse Radeon RX 6600",
+    imagen:
+      "../recursos/img/productos/gpu/rx6600/sapphire-gpu-transformed.webp",
+    vram: "8GB",
+    chipset: "Rx 6600",
+    precio: 200,
+    descripcion:
+      "Tarjeta gráfica compacta y potente con diseño elegante y gran rendimiento en juegos de última generación",
+  },
+  rx6600_powercolor: {
+    nombre: "PowerColor Red Devil Radeon RX 6600",
+    imagen:
+      "../recursos/img/productos/gpu/rx6600/powercolor-removebg-preview.webp",
+    vram: "8GB",
+    chipset: "Rx 6600",
+    precio: 200,
+    descripcion:
+      "Tarjeta gráfica para jugadores entusiastas, con overclocking de fábrica y características avanzadas de enfriamiento",
+  },
+  rx6600_xfx: {
+    nombre: "XFX Speedster Radeon RX 6600",
+    imagen: "../recursos/img/productos/gpu/rx6600/xfx-gpu.png",
+    vram: "8GB",
+    chipset: "Rx 6600",
+    precio: 200,
+    descripcion:
+      "Tarjeta gráfica diseñada para un rendimiento óptimo en juegos de alta exigencia, con 8GB de VRAM y diseño personalizado",
+  },
+  rx6600_xt_asus: {
+    nombre: "ASUS Radeon RX 6600 XT ROG Strix",
+    imagen: "../recursos/img/productos/gpu/rx6600_xt/asus-gpu.webp",
+    vram: "8GB",
+    chipset: "Rx 6600 XT",
+    precio: 250,
+    descripcion:
+      "Potente tarjeta gráfica para juegos de alta gama, diseñada con el exclusivo sistema de enfriamiento ROG Strix y 8GB de memoria GDDR6 para una experiencia de juego inmersiva",
+  },
+  rx6600_xt_msi: {
+    nombre: "MSI Radeon RX 6600 XT Gaming X",
+    imagen: "../recursos/img/productos/gpu/rx6600_xt/msi-gpu.png",
+    vram: "8GB",
+    chipset: "Rx 6600 XT",
+    precio: 250,
+    descripcion:
+      "Tarjeta gráfica de alta calidad con un rendimiento excepcional en resoluciones 1080p y 1440p, equipada con 8GB de VRAM GDDR6 y tecnología de enfriamiento avanzada",
+  },
+  rx6600_xt_gigabyte: {
+    nombre: "GIGABYTE Radeon RX 6600 XT AORUS Master",
+    imagen: "../recursos/img/productos/gpu/rx6600_xt/gigabyte-gpu.webp",
+    vram: "8GB",
+    chipset: "Rx 6600 XT",
+    precio: 250,
+    descripcion:
+      "Tarjeta gráfica de la serie AORUS Master con diseño personalizado y componentes de alta calidad para un rendimiento y estabilidad superiores en juegos exigentes",
+  },
+  rx6600_xt_sapphire: {
+    nombre: "Sapphire Pulse Radeon RX 6600 XT",
+    imagen: "../recursos/img/productos/gpu/rx6600_xt/sapphire-gpu.png",
+    vram: "8GB",
+    chipset: "Rx 6600 XT",
+    precio: 250,
+    descripcion:
+      "Tarjeta gráfica compacta con diseño elegante y gran eficiencia energética, perfecta para juegos en resoluciones 1080p con 8GB de memoria GDDR6",
+  },
+  rx6600_xt_powercolor: {
+    nombre: "PowerColor Red Devil Radeon RX 6600 XT",
+    imagen: "../recursos/img/productos/gpu/rx6600_xt/power_color-gpu.png",
+    vram: "8GB",
+    chipset: "Rx 6600 XT",
+    precio: 250,
+    descripcion:
+      "Tarjeta gráfica diseñada para jugadores entusiastas, con overclocking de fábrica y sistema de enfriamiento avanzado para un rendimiento máximo",
+  },
+  rx6600_xt_xfx: {
+    nombre: "XFX Speedster QICK 308 Radeon RX 6600 XT",
+    imagen: "../recursos/img/productos/gpu/rx6600_xt/xfx-gpu.jpg",
+    vram: "8GB",
+    chipset: "Rx 6600 XT",
+    precio: 250,
+    descripcion:
+      "Tarjeta gráfica con diseño moderno y compacto, ideal para sistemas de factor de forma pequeño (SFF), con 8GB de VRAM GDDR6 y alto rendimiento en juegos",
+  },
+};
+
+export const cpu = {
+  ryzen_5600x: {
+    nombre: "Ryzen 5 5600x",
+    imagen: "../recursos/img/productos/cpu/amd/ryzen_5_5600x.png",
+    cores: 6,
+    precio: 300,
+    descripcion:
+      "Procesador Ryzen 5 de 6 núcleos y 12 hilos con alto rendimiento en aplicaciones y juegos para usuarios exigentes.",
+    socket: "AM4",
+    empresa: "AMD",
+  },
+  ryzen_5800x: {
+    nombre: "Ryzen 7 5800x",
+    imagen: "../recursos/img/productos/cpu/amd/ryzen_7_5800x.png",
+    cores: 8,
+    precio: 400,
+    descripcion:
+      "Procesador Ryzen 7 de 8 núcleos y 16 hilos diseñado para tareas de alta carga de trabajo y juegos intensivos.",
+    socket: "AM4",
+    empresa: "AMD",
+  },
+  ryzen_5900x: {
+    nombre: "Ryzen 9 5900x",
+    imagen: "../recursos/img/productos/cpu/amd/ryzen_9_5900x.png",
+    cores: 12,
+    precio: 600,
+    descripcion:
+      "Procesador Ryzen 9 de 12 núcleos y 24 hilos para una potencia excepcional en aplicaciones profesionales y experiencias de juego inigualables.",
+    socket: "AM4",
+    empresa: "AMD",
+  },
+  ryzen_5600g: {
+    nombre: "Ryzen 5 5600g",
+    imagen: "../recursos/img/productos/cpu/amd/ryzen_5_5600g.png",
+    cores: 6,
+    precio: 250,
+    descripcion:
+      "Procesador Ryzen 5 con gráficos integrados Radeon, ideal para sistemas compactos y aplicaciones multimedia.",
+    socket: "AM4",
+    empresa: "AMD",
+  },
+  ryzen_5700g: {
+    nombre: "Ryzen 7 5700g",
+    imagen: "../recursos/img/productos/cpu/amd/ryzen_7_5700g.png",
+    cores: 8,
+    precio: 350,
+    descripcion:
+      "Procesador Ryzen 7 con gráficos Radeon para una experiencia de juego sin problemas y rendimiento en aplicaciones multitarea.",
+    socket: "AM4",
+    empresa: "AMD",
+  },
+  intel_10600k: {
+    nombre: "Intel Core Intel Core 10600k",
+    imagen: "../recursos/img/productos/cpu/intel/10600k.png",
+    cores: 6,
+    precio: 250,
+    descripcion:
+      "Procesador de 10a generación desbloqueado para overclocking, con 6 núcleos y 12 hilos para un rendimiento excepcional en juegos y tareas intensivas.",
+    socket: "LGA1200",
+    empresa: "Intel",
+  },
+  intel_10700kf: {
+    nombre: "Intel Core I7 10700kf",
+    imagen: "../recursos/img/productos/cpu/intel/10700kf.png",
+    cores: 8,
+    precio: 350,
+    descripcion:
+      "Procesador de 10a generación desbloqueado con 8 núcleos y 16 hilos para un rendimiento potente en juegos y aplicaciones con alta carga de trabajo.",
+    socket: "LGA1200",
+    empresa: "Intel",
+  },
+  intel_10900k: {
+    nombre: "Intel Core I9 10900k",
+    imagen: "../recursos/img/productos/cpu/intel/10900k.png",
+    cores: 10,
+    precio: 500,
+    descripcion:
+      "Procesador de 10a generación desbloqueado con 10 núcleos y 20 hilos, ideal para juegos y tareas de edición y renderizado intensivas.",
+    socket: "LGA1200",
+    empresa: "Intel",
+  },
+  intel_11600k: {
+    nombre: "Intel Core I5 11600k",
+    imagen: "../recursos/img/productos/cpu/intel/11600k.png",
+    cores: 6,
+    precio: 280,
+    descripcion:
+      "Procesador de 11a generación desbloqueado con 6 núcleos y 12 hilos, perfecto para jugadores y creadores de contenido.",
+    socket: "LGA1200",
+    empresa: "Intel",
+  },
+  intel_11700k: {
+    nombre: "Intel Core I7 11700k",
+    imagen: "../recursos/img/productos/cpu/intel/11700k.png",
+    cores: 8,
+    precio: 400,
+    descripcion:
+      "Procesador de 11a generación desbloqueado con 8 núcleos y 16 hilos para un rendimiento excepcional en tareas exigentes.",
+    socket: "LGA1200",
+    empresa: "Intel",
+  },
+};
---
+++ tests.js
@@ -0,0 +1,52 @@
+import { gpu, cpu } from "./data.js";
+
+const productos = { ...gpu, ...cpu };
+
+let search = document.getElementById("search");
+search.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    const termino_buscado = search.value;
+
+    localStorage.setItem("termino_buscado", termino_buscado);
+    console.log(termino_buscado);
+    window.location.href = "../pag2.html";
+  }
+});
+
+const producto_buscado = localStorage.getItem("termino_buscado");
+
+function realizarBusqueda(termino) {
+  let resultadoBusqueda = [];
+
+  Object.entries(productos).forEach(([clave, otro]) => {
+    if (
+      otro.nombre.toLowerCase().includes(termino) ||
+      otro.descripcion.toLowerCase().includes(termino)
+    ) {
+      resultadoBusqueda.push(productos[clave]);
+    }
+  });
+  return resultadoBusqueda;
+}
+
+let mostrar = realizarBusqueda(producto_buscado);
+
+const testeo = document.getElementById("here");
+testeo.classList.add("fondo");
+
+if (mostrar.length === 0) {
+  const error_msg = document.createElement("p");
+  error_msg.innerText = "404 NOT FOUND";
+  testeo.appendChild(error_msg);
+} else {
+  for (let i = 0; i < mostrar.length; i++) {
+    const show_nombre = document.createElement("p");
+    show_nombre.innerText = mostrar[i].nombre;
+    testeo.appendChild(show_nombre);
+    const show_precio = document.createElement("p");
+    show_precio.innerText = mostrar[i].precio;
+    testeo.appendChild(show_precio);
+  }
+}
+
+console.log(mostrar);
